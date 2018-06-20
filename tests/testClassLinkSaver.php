ade1c78f67014d1741f5166d911e2645f57424ed
bd <bd@TROVARIT-195-BD.Trovaritoffice.local>
2018-06-20 17:53:26 +0200

    erster Test

--- tests/testClassLinkSaver.php
+++ tests/testClassLinkSaver.php
@@ -1,1 +1,13 @@
 <?php
+use PHPUnit\Framework\TestCase;
+
+class LinkSaverTest extends TestCase
+{
+    public function testConstructClass()
+    {
+        $linksaver = new LinkSaver();
+        $this->assert(is_Class($linksaver, "LinkSaver"));
+    }
+    
+}
+
